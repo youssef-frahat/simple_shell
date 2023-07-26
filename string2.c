a0dde2ba177ed1addb03d759e4ae217d8ce1072a
youssef frahat <108248554+youssef-frahat@users.noreply.github.com>
2023-07-26 04:42:45 +0300

    string2.c

--- string2.c
+++ string2.c
@@ -3,7 +3,7 @@
 /**
  * _strdup - duplicates a str in the heap memory.
  * @s: Type char pointer str.
- * by youssef and hassan.
+ * by youssef
  * Return: duplicated str.
  */
 char *_strdup(const char *s)
